e4eeec23f78ee97a33a9925b639e27ef50de5309
britannic <github@empirecreekcircle.com>
2016-03-30 17:13:55 -0700

    Updated README.md/Tests

--- testutils.go
+++ testutils.go
@@ -17,15 +17,6 @@ func Assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
 	}
 }
 
-// OK fails the test if an err is not nil.
-func OK(tb testing.TB, err error) {
-	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
-		tb.Fail()
-	}
-}
-
 // Equals fails the test if exp is not equal to act.
 func Equals(tb testing.TB, exp, act interface{}) {
 	if !reflect.DeepEqual(exp, act) {
@@ -47,6 +38,15 @@ func NotEquals(tb testing.TB, exp, act interface{}) {
 // NotOK fails the test if an err is nil.
 func NotOK(tb testing.TB, err error) {
 	if err == nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.Fail()
+	}
+}
+
+// OK fails the test if an err is not nil.
+func OK(tb testing.TB, err error) {
+	if err != nil {
 		_, file, line, _ := runtime.Caller(1)
 		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
 		tb.Fail()
